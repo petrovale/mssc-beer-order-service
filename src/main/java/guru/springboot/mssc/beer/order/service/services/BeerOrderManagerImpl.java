eb8569ea47ec9f3fd0cf38a56807abe595ad0139
petrovale <leha.isakov@yandex.ru>
2020-02-22 14:32:37 +0300

    Adding Interceptor

--- src/main/java/guru/springboot/mssc/beer/order/service/services/BeerOrderManagerImpl.java
+++ src/main/java/guru/springboot/mssc/beer/order/service/services/BeerOrderManagerImpl.java
@@ -4,6 +4,7 @@ import guru.springboot.mssc.beer.order.service.domain.BeerOrder;
 import guru.springboot.mssc.beer.order.service.domain.BeerOrderEventEnum;
 import guru.springboot.mssc.beer.order.service.domain.BeerOrderStatusEnum;
 import guru.springboot.mssc.beer.order.service.repositories.BeerOrderRepository;
+import guru.springboot.mssc.beer.order.service.sm.BeerOrderStateChangeInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
@@ -17,8 +18,10 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class BeerOrderManagerImpl implements BeerOrderManager {
 
+  public static final String ORDER_ID_HEADER = "ORDER_ID_HEADER";
   private final StateMachineFactory<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachineFactory;
   private final BeerOrderRepository beerOrderRepository;
+  private final BeerOrderStateChangeInterceptor beerOrderStateChangeInterceptor;
 
   @Transactional
   @Override
@@ -35,6 +38,7 @@ public class BeerOrderManagerImpl implements BeerOrderManager {
     StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> sm = build(beerOrder);
 
     Message msg = MessageBuilder.withPayload(eventEnum)
+        .setHeader(ORDER_ID_HEADER, beerOrder.getId().toString())
         .build();
 
     sm.sendEvent(msg);
@@ -47,6 +51,7 @@ public class BeerOrderManagerImpl implements BeerOrderManager {
 
     sm.getStateMachineAccessor()
         .doWithAllRegions(sma -> {
+          sma.addStateMachineInterceptor(beerOrderStateChangeInterceptor);
           sma.resetStateMachine(new DefaultStateMachineContext<>(beerOrder.getOrderStatus(), null, null, null));
         });
 
