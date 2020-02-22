d96ee5fe05c45efc53a4c199f7d656e3c55baa0b
petrovale <leha.isakov@yandex.ru>
2020-02-22 12:42:01 +0300

    add validation transaction

--- src/main/java/guru/springboot/mssc/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/guru/springboot/mssc/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 @Configuration
 @EnableStateMachineFactory
@@ -23,5 +24,20 @@ public class BeerOrderStateMachineConfig extends
         .end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
         .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
         .end(BeerOrderStatusEnum.ALLOCATION_EXCEPTION);
+  }
+
+  @Override
+  public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum,
+          BeerOrderEventEnum> transitions) throws Exception {
+    transitions.withExternal()
+        .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.NEW)
+        .event(BeerOrderEventEnum.VALIDATE_ORDER)
+        //todo add validation action here
+        .and().withExternal()
+        .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATED)
+        .event(BeerOrderEventEnum.VALIDATION_PASSED)
+        .and().withExternal()
+        .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+        .event(BeerOrderEventEnum.VALIDATION_FAILED);
   }
 }
